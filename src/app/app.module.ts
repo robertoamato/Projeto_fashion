d84c55a8d836fda8022bf1a0b013f1ee12332b19
Roberto amato neto <66858040+robertoamato@users.noreply.github.com>
2022-07-30 00:45:07 -0300

    Site simples voltado para a moda

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Componente1Component } from './componente1/componente1.component';
+import { Componente2Component } from './componente2/componente2.component';
+import { Componente3Component } from './componente3/componente3.component';
+import { Componente4Component } from './componente4/componente4.component';
+import { Componente5Component } from './componente5/componente5.component';
+import { Componente6Component } from './componente6/componente6.component';
+import { Componente7Component } from './componente7/componente7.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Componente1Component,
+    Componente2Component,
+    Componente3Component,
+    Componente4Component,
+    Componente5Component,
+    Componente6Component,
+    Componente7Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
